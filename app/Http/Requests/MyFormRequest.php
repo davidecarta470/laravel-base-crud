cfb8039e6f11ebc86723608c8d6ed2583beb949e
davidecarta470 <cartadavi@gmail.com>
2022-01-31 22:48:56 +0100

    create input value

--- app/Http/Requests/MyFormRequest.php
+++ app/Http/Requests/MyFormRequest.php
@@ -4,7 +4,7 @@ namespace App\Http\Requests;
 
 use Illuminate\Foundation\Http\FormRequest;
 
-class MyRequest extends FormRequest
+class MyFormRequest extends FormRequest
 {
     /**
      * Determine if the user is authorized to make this request.
--- resources/views/series/create.blade.php
+++ resources/views/series/create.blade.php
@@ -27,6 +27,7 @@
             name="title" 
             id="title" 
             aria-describedby="emailHelp"
+            value="{{old('title')}}"
            >
 
           @error('title')
@@ -39,30 +40,30 @@
         </div>
         <div class="mb-3">
           <label for="exampleInputPassword1" class="form-label">immagine</label>
-          <input type="text" class="form-control" name="img" id="img">
+          <input type="text" value="{{old('img')}}" class="form-control" name="img" id="img">
         </div>
         <div class="mb-3">
           <label for="exampleInputPassword1" class="form-label">Descrizione</label>
-          <input type="text" class="form-control" name="description" id="description">
+          <input type="text" value="{{old('description')}}" class="form-control" name="description" id="description">
         </div>
         <div class="mb-3">
           <label for="exampleInputPassword1" class="form-label">Serie</label>
-          <input type="text" class="form-control" name="series" id="series">
+          <input type="text" value="{{old('series')}}" class="form-control" name="series" id="series">
         </div>
         <div class="mb-3">
           <label for="exampleInputPassword1" class="form-label">Prezzo</label>
-          <input type="text" class="form-control @error ('price') is-invalid @enderror" name="price" id="price">
+          <input type="text" value="{{old('price')}}" class="form-control @error ('price') is-invalid @enderror" name="price" id="price">
           @error('price') 
             <p>{{$message}} </p>
           @enderror  
         </div>
         <div class="mb-3">
           <label for="exampleInputPassword1" class="form-label">Data di uscita</label>
-          <input type="text" class="form-control" name="sale_date" id="sale_date">
+          <input type="text" value="{{old('sale_date')}}" class="form-control" name="sale_date" id="sale_date">
         </div>
         <div class="mb-3">
           <label for="exampleInputPassword1" class="form-label">Tipo</label>
-          <input type="text" class="form-control" name="type" id="type">
+          <input type="text" value="{{old('type')}}" class="form-control" name="type" id="type">
         </div>
      
         <button type="submit" class="btn btn-primary">Submit</button>
